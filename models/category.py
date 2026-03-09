b0d89f2c0decb9a53585affac084b58b1571f9bb
jeim <159331654+Jeimtax@users.noreply.github.com>
2026-03-09 00:41:25 -0500

    Edited attributes for grade level and subject

--- models/category.py
+++ models/category.py
@@ -1,11 +1,17 @@
 class Category:
-    def __init__(self, name: str, description: str=""):
-        self.name = name.strip().title()
-        self.description = description
+    def __init__(self, grade_level: str, subject: str):
+        if not grade_level.strip():
+            raise ValueError("Grade level cannot be empty")
+        
+        if not subject.strip():
+            raise ValueError("Grade level cannot be empty")
+        
+        self.grade_level = grade_level.strip().title()
+        self.subject = subject.strip().title()
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.grade_level} - {self.subject}"
     
     def __repr__(self):
         """For printing list items"""
-        return f"{self.name}"
+        return f"{self.grade_level} - {self.subject}"
