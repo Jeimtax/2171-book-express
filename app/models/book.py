21ad4ed7e39e1a3d5354390fce0e611a2914124c
jeim <159331654+Jeimtax@users.noreply.github.com>
2026-03-25 17:47:05 -0500

    Updated classes

--- app/models/book.py
+++ app/models/book.py
@@ -9,27 +9,32 @@ class Book (db.Model):
     condition = db.Column(db.String(80), nullable=False)
     grade = db.Column(db.String(80))
     subject = db.Column(db.String(80))
+    quantity = db.Column(db.Integer, nullable=False, default=1)
     
 
-    def __init__(self, title, author, price, condition, grade=None, subject=None):
+    def __init__(self, title, author, price, condition, grade, subject, quantity=1):
         self.title = title
         self.author = author
         self.price = price
         self.condition = condition
         self.grade = grade
         self.subject = subject
+        self.quantity = quantity
 
     def to_dict(self):
         return {
             'id': self.id,
             'title': self.title,
             'author': self.author,
+            'price': self.price,
+            'condition': self.condition,
             'grade': self.grade,
             'subject': self.subject,
-            'price': self.price,
-            'condition': self.condtion,
             'quantity': self.quantity
         }
-        
+
+
+    def __repr__(self):
+        return f' Title:{self.title}\n Author:{self.author}\n Grade: {self.grade}\n Subject: {self.subject}\n ${self.price}\n Condition:{self.condition}\n Quantity: {self.quantity}\n'  
     
 
--- app/models/inventorymanager.py
+++ app/models/inventorymanager.py
@@ -3,14 +3,17 @@ from app import db
 
 class InventoryManager:
     
-    def add_book(self, title, author, price, condition, grade, subject):
-        book = Book( title, author, price, condition, grade, subject)
+    def add_book(self, title, author, price, condition, grade, subject, quantity):
+        """Adds a Book to the database"""
+        book = Book( title, author, price, condition, grade, subject, quantity)
         db.session.add(book)
         db.session.commit()
         return book
     
-    def manage_book_info(self, id, title=None, author=None, price=None, condition=None, grade=None, subject=None):
+    def manage_book_info(self, id, title=None, author=None, price=None, condition=None, grade=None, subject=None, quantity=None):
+        """Allows to edit a singular or multiple attributes of a Book"""
         book = Book.query.get_or_404(id)
+
 
         if title is not None:
             book.title = title
@@ -29,7 +32,38 @@ class InventoryManager:
 
         if subject is not None:
             book.subject = subject
-        db.session.commit()
 
+        if quantity is not None:
+            book.quantity = quantity
+        db.session.commit()
+        return book
+
+    def search_books(self, title=None, author=None, grade=None, subject=None, max_price=None):
+        query = Book.query
+
+        if title:
+            query = query.filter(Book.title == title)
+
+        if author:
+            query = query.filter(Book.author == author)
+
+        if grade:
+            query = query.filter(Book.grade == grade)
+
+        if subject:
+            query = query.filter(Book.subject == subject)
+
+        if max_price is not None and max_price != "":
+            try:
+                price_value = float(max_price)
+                query = query.filter(Book.price <= price_value)
+            except ValueError:
+                pass
+
+
+
+        # Real-time inventory levels come directly from the live DB
+        results = query.order_by(Book.title.asc()).all()
+        return results
     
     
