a53efb9a7120d05600d3f89a8243772daa45765e
Tristan Martin <pwmarim44@gmail.com>
2026-04-09 08:26:33 -0500

    Added new dashboard route, created dashboard webpage and added book totalling functionality

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from .config import Config
 from flask_migrate import Migrate
 
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -17,12 +18,13 @@ from app.models.inventory_adjustment import InventoryAdjustment
 from app.models.supplier import Supplier
 from app.models.order import Order
 
-from app.routes.books_routes import books_bp
+
 from app.routes.csv_routes import upload_bp
 from app.routes.inventory_routes import inventory_bp
 from app.routes.orders_routes import orders_bp
+from app.routes.dashboard_routes import dashboard_bp
 
-app.register_blueprint(books_bp)
 app.register_blueprint(upload_bp)
 app.register_blueprint(inventory_bp)
 app.register_blueprint(orders_bp)
+app.register_blueprint(dashboard_bp)
